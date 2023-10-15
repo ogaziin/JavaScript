78e50cbffc754b62c496c078573b7a1159f368cc
ogaziin <147756878+ogaziin@users.noreply.github.com>
2023-10-15 19:40:25 -0300

    Site de Cadastro criado.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const person = {
-    name: 'Jane',
-    age: 21,
-}
+const submitButton = document.querySelector('#submit-button');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const errorMessage = document.querySelector('.msg');
+const listItems = document.querySelector('.items');
 
-for (property in person) {
-    console.log(property);
-}
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+
+    if (nameValue === '' || emailValue === '') {
+        errorMessage.textContent = 'Please, fill out all the fields!'
+        errorMessage.style.color = 'red';
+        return 1;
+    } else {
+        errorMessage.textContent = '';
+    }
+
+    listItems.style.display = 'block';
+
+    listItems.firstElementChild.textContent = nameValue;
+    listItems.lastElementChild.textContent = emailValue;
+});
