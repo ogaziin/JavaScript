a9ca668f3917873bef29460da98d0c2661c16955
ogaziin <147756878+ogaziin@users.noreply.github.com>
2023-10-15 17:37:40 -0300

    Trabalhando com loop for in.

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-const numbers = [1, 2, 3, 4, 5];
+const person = {
+    name: 'Jane',
+    age: 21,
+}
 
-const numbersMultipliedByTwo = numbers.map(function(number) {
-    return number * 2;
-});
-
-console.log(numbersMultipliedByTwo);
-
-// -------------
-
-const ages = [8, 13, 27, 30, 40];
-
-const evenAges = ages.filter(function(age) {
-    return age % 2 == 0;
-});
-
-// -----------
-
-const sumAges = ages.reduce(function(age, accumulator) {
-    return accumulator + age;
-}, 0);
-
-console.log(sumAges)
+for (property in person) {
+    console.log(property);
+}
