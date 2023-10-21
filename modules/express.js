c13e099526563bc3be6957df086399166a401be4
ogaziin <147756878+ogaziin@users.noreply.github.com>
2023-10-21 17:18:34 -0300

    Testado módulo http e express.

---
+++ modules/express.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/home", (req, res) => {
+  res.status(200).send("<h1>This is the Home Page!</h1>");
+});
+
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "John Doe",
+      email: "john@doe.com",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane@doe.com",
+    },
+  ];
+
+  res.status(200).json(users);
+});
+
+app.listen(8080, () => console.log(`Rodando na porta ${port}`));
--- modules/http.js
+++ modules/http.js
@@ -1,28 +1,50 @@
-const http = require("http");
+// const http = require("http");
 
-const port = 8080;
+// const port = 8080;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/home") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Home Page</h1>");
-  }
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/home") {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end("<h1>Home Page</h1>");
+//   }
 
-  if (req.url === "/users") {
-    const users = [
-      {
-        name: "John Doe",
-        email: "john@doe.com",
-      },
-      {
-        name: "Jane Doe",
-        email: "jane@doe.com",
-      },
-    ];
+//   if (req.url === "/users") {
+//     const users = [
+//       {
+//         name: "John Doe",
+//         email: "john@doe.com",
+//       },
+//       {
+//         name: "Jane Doe",
+//         email: "jane@doe.com",
+//       },
+//     ];
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
-  }
-});
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(JSON.stringify(users));
+//   }
 
-server.listen(port, () => console.log(`Rodando na porta ${port}!`));
+//   if (req.url === "/admin") {
+//     res.writeHead(401, { "Content-Type": "text/html" });
+//     res.end(
+//       "<h1 style='text-align: center;'>Forbidden</h1>\n<p style='text-align: center;'>You cannot acess this site."
+//     );
+//   }
+// });
+
+// server.listen(port, () => console.log(`Rodando na porta ${port}!`));
+
+// const http = require("http"); // - Importa o módulo HTTP.
+// const port = 8080; // - Porta usada no servidor.
+
+// const server = http.createServer((req, res) => {
+//   // - Cria o servidor HTTP.
+//   if (req.url === "/admin") {
+//     res.writeHead(403, { "Content-Type": "text/html" });
+//     res.end(
+//       "<title>Forbidden</title>\n<h1>Forbidden</h1>\n<p>You cannot acess this page</p>"
+//     );
+//   }
+// });
+
+// server.listen(port, () => console.log(`Rodando na porta ${port}`));
