ddc2b04f445b42fd5d7e265f4c6e792810e062d5
ogaziin <147756878+ogaziin@users.noreply.github.com>
2023-10-21 17:45:53 -0300

    Feito conexão com banco de dados.

--- modules/express.js
+++ modules/express.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const app = express();
-const port = 8080;
+// const express = require("express");
+// const app = express();
+// const port = 8080;
 
-app.get("/home", (req, res) => {
-  res.status(200).send("<h1>This is the Home Page!</h1>");
-});
+// app.get("/home", (req, res) => {
+//   res.status(200).send("<h1>This is the Home Page!</h1>");
+// });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "John Doe",
-      email: "john@doe.com",
-    },
-    {
-      name: "Jane Doe",
-      email: "jane@doe.com",
-    },
-  ];
+// app.get("/users", (req, res) => {
+//   const users = [
+//     {
+//       name: "John Doe",
+//       email: "john@doe.com",
+//     },
+//     {
+//       name: "Jane Doe",
+//       email: "jane@doe.com",
+//     },
+//   ];
 
-  res.status(200).json(users);
-});
+//   res.status(200).json(users);
+// });
 
-app.get("/admin", (req, res) => {
-  res.status(403).send("<title>Forbidden</title>\n<h1>Forbidden</h1>");
-});
+// app.get("/admin", (req, res) => {
+//   res.status(403).send("<title>Forbidden</title>\n<h1>Forbidden</h1>");
+// });
 
-app.listen(8080, () => console.log(`Rodando na porta ${port}`));
+// app.listen(8080, () => console.log(`Rodando na porta ${port}`));
